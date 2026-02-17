ab8b579d342502030578d7be2071b05056bd9d9b
Afik <afikrahman28@gmail.com>
2026-02-18 01:06:12 +0600

    done with first boiler plate

--- script/index.js
+++ script/index.js
@@ -1,8 +1,18 @@
-
-
-document.getElementById('start').addEventListener('click',()=>{
-   const login=document.getElementById('login');
-   login.classList.remove('hidden')
-   const start=document.getElementById('start');
-   start.classList.add('hidden')
+document.getElementById("start").addEventListener("click", () => {
+  const login = document.getElementById("login");
+  login.classList.remove("hidden");
+  const start = document.getElementById("start");
+  start.classList.add("hidden");
+});
+document.getElementById('login').addEventListener('click',()=>{
+   const faq=document.getElementById('faq');
+   faq.classList.remove('hidden');
+   const logout=document.getElementById('logout');
+   logout.classList.remove('hidden')
+   const third=document.getElementById('thirdsec');
+   third.classList.remove('hidden')
+   const fourth=document.getElementById('fourth');
+   fourth.classList.remove('hidden')
+   const footer=document.getElementById('footer');
+   footer.classList.remove('hidden')
 })
