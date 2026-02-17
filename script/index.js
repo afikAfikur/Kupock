e3eb860296cd2d0121335811cb1f75ab9a04724b
Afik <afikrahman28@gmail.com>
2026-02-17 21:36:18 +0600

    first commit

---
+++ script/index.js
@@ -0,0 +1,8 @@
+
+
+document.getElementById('start').addEventListener('click',()=>{
+   const login=document.getElementById('login');
+   login.classList.remove('hidden')
+   const start=document.getElementById('start');
+   start.classList.add('hidden')
+})
