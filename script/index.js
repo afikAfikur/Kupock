c5d15ca24db6d59213033c06b7eeb48e1a304b34
Afik <afikrahman28@gmail.com>
2026-02-25 22:57:10 +0600

    done with faq

--- script/index.js
+++ script/index.js
@@ -16,3 +16,23 @@ document.getElementById('login').addEventListener('click',()=>{
    const footer=document.getElementById('footer');
    footer.classList.remove('hidden')
 })
+document.getElementById('logout').addEventListener('click',()=>{
+  const login = document.getElementById("login");
+  login.classList.add("hidden");
+  const start = document.getElementById("start");
+  start.classList.remove("hidden");
+  const faq=document.getElementById('faq');
+   faq.classList.add('hidden');
+   const logout=document.getElementById('logout');
+   logout.classList.add('hidden')
+   const third=document.getElementById('thirdsec');
+   third.classList.add('hidden')
+   const fourth=document.getElementById('fourth');
+   fourth.classList.add('hidden')
+   const footer=document.getElementById('footer');
+   footer.classList.add('hidden')
+})
+document.getElementById('faq').addEventListener('click',()=>{
+  const faqSection=document.getElementById('fourth')
+  faqSection.scrollIntoView({ behavior: 'smooth' });
+})
